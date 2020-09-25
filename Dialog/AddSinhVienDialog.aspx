5232bec77a8276d87697c5dc512843e1b8430e03
tuanpth1909 <60849085+tuanpth1909@users.noreply.github.com>
2020-09-25 14:26:22 +0700

    Add project files.

---
+++ Dialog/AddSinhVienDialog.aspx
@@ -0,0 +1,96 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/LayoutPage/LayoutPage.Master" AutoEventWireup="true" CodeBehind="AddSinhVienDialog.aspx.cs" Inherits="AppQuanLiSinhVien.Dialog.AddSinhVienDialog" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div id="AddDialog">
+        <h3 class="text-center">Add student profile</h3>
+        <hr class="bg-white" />
+        <div class="row" id="SinhVienDialog">
+            <form>
+                <div class="col-6">
+                    <%--Name--%>
+                    <div class="form-group">
+                        <label>Name:</label>
+                        <input type="text" name="name" id="txtName" class="w-75 float-right" value="" required />
+                    </div>
+                    <%--Birth Day--%>
+                    <div class="form-group">
+                        <label>Birth Day:</label>
+                        <input type="date" name="date" id="datBirthDay" class="w-75 float-right" value="" required />
+                    </div>
+                    <%--Sex--%>
+                    <div class="form-group">
+                        <label class="w-25">Sex:</label>
+                        <input type="radio" name="sex" value="Male" checked />
+                        Male
+                  
+                            <input type="radio" name="sex" value="Female" class="" />
+                        Female
+                    </div>
+                    <%--Address--%>
+                    <div class="form-group">
+                        <label>Address:</label>
+                        <input type="text" name="add" id="txtAddress" class="w-75 float-right" value="" required />
+                    </div>
+                    <%--Tel--%>
+                    <div class="form-group">
+                        <label>Tel:</label>
+                        <input type="number" name="tel" id="numTel" class="w-75 float-right" value="" required />
+                    </div>
+                    <%--Email--%>
+                    <div class="form-group">
+                        <label>Email:</label>
+                        <input type="email" name="email" id="emlEmail" class="w-75 float-right" value="" required />
+                    </div>
+                </div>
+                <div class="col-6">
+                    <%--Personal biography--%>
+                    <div class="form-group">
+                        <label>Personal biography:</label>
+                        <textarea id="txtPersonal" name="txtPersonal" class="w-100 float-right"></textarea>
+                    </div>
+                    <label for="vehicle1" class="mt-1">Favorite sport:</label><br>
+                    <div class="form-group ">
+                        <div class="row">
+                            <div class="col-6">
+                                <input type="checkbox" id="chekSoccer" name="sport" value="Soccer">
+                                <label for="vehicle1">Soccer</label><br>
+                                <input type="checkbox" id="chekBadminton" name="sport" value="Badminton">
+                                <label for="vehicle2">Running</label><br>
+                                <input type="checkbox" id="chekSwimming" name="sport" value="Swimming">
+                                <label for="vehicle3">Swimming</label><br>
+                            </div>
+                            <div class="col-6">
+                                <input type="checkbox" id="checkBasketball" name="sport" value="Basketball">
+                                <label for="vehicle3">Basketball</label><br>
+                                <input type="checkbox" id="checkJogging" name="sport" value="Jogging">
+                                <label for="vehicle3">Jogging</label><br>
+                                <input type="checkbox" id="checkBoxing" name="sport" value="Boxing">
+                                <label for="vehicle3">Boxing</label>
+                            </div>
+                        </div>
+
+                        <br>
+                    </div>
+                    <%--Class--%>
+                    <div class="form-group">
+                        <label>Class:</label>
+                        <select name="classes" id="classes" class="w-75 float-right" style="padding-top: -50px">
+                            <option value="T1909M">T1909M</option>
+                            <option value="T1908M">T1908M</option>
+                            <option value="T1907M">T1907M</option>
+                            <option value="T1906M">T1906M</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="btnAction">
+                    <button type="button" name="btnSave" id="btnSave" class=" text-center"><strong>LƯU</strong></button>
+                    <button type="button" name="btnUpdate" id="btnUpdate" class=" text-center" style="display: none"><strong>CẬP NHẬT</strong></button>
+                    <button type="button" name="btnSave" id="btnCancel" class=" text-center"><strong>HỦY</strong></button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <button type="button" id="btnAdd" class="btn btn-primary">Add</button>
+</asp:Content>
---
+++ js/myMain.js
@@ -0,0 +1,253 @@
+﻿$(document).ready(function () {
+
+    //$('#AddDialog').dialog({
+    //    autoOpen: false,
+    //    height: 400,
+    //    width: 550,
+    //    modal: true,
+    //    show: {
+    //        effect: 'drop',
+    //        duration: 200,
+    //    },
+    //    hide: {
+    //        effect: 'drop',
+    //        duration: 200,
+    //    },
+
+    //});
+    //$('#btnAdd').click(function () {
+    //    $('#AddDialog').dialog('open');
+    //});
+
+    //$("#btnAdd").click(function () {
+    //    //debugger
+    //    $.post("/Dialog/AppSinhVienDialog.aspx", function (data) {
+    //        //debugger;
+    //        $("#Dialog").html(data);
+    //        $('#Dialog').dialog({
+    //            autoOpen: false,
+    //            height: 900,
+    //            width: 700,
+    //            modal: true,
+    //            show: {
+    //                effect: 'scale',
+    //                duration: 250,
+    //            },
+    //            hide: {
+    //                effect: 'drop',
+    //                duration: 200,
+    //            }
+    //        });
+    //        $('#Dialog').dialog('open');
+    //    });
+    //});
+
+    //Thêm mới
+    addStudents();
+
+    //Xóa
+    deleteStudents();
+
+    //Tìm kiếm
+    searchStudents();
+
+    //Hủy
+    cancelStudents();
+
+    //Chỉnh sửa
+    editStudents();
+
+    
+});
+
+//Hàm xóa hồ sơ sinh viên
+function deleteStudents() {
+    $('a[name=delete]').click(function () {
+        $(this).closest('tr').remove();
+    });
+}
+
+//Hàm tìm kiếm hồ sơ 
+function searchStudents() {
+    $('#search').on('keyup', function () {
+        let keyword = $(this).val().toLowerCase();
+        $('tbody tr').filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(keyword) > -1);
+        });
+    });
+}
+
+//Hàm thêm mới hồ sơ sinh vien
+function addStudents() {
+
+    $('#btnSave').click(function () {
+        debugger;
+        //validateStudents();
+        //lay gia tri tu cac input
+        let name = $('#txtName').val();
+        let dob = $('#datBirthDay').val();
+        let sex = $('input[name=sex]:checked').val();
+        let add = $('#txtAddress').val();
+        let tel = $('#numTel').val();
+        let email = $('#emlEmail').val();
+        let personal = $('#txtPersonal').val();
+        let selectedSport = [];
+        $('input[name=sport]:checked').each(function () {
+            selectedSport.push(this.value);
+        });
+        let classes = $("#classes option:selected").val();
+
+        //chen du lieu vao table
+        $('tbody').prepend("<tr>" +
+            "<td>" + name + "</td>" +
+            "<td>" + dob + "</td>" +
+            "<td>" + sex + "</td>" +
+            "<td>" + add + "</td>" +
+            "<td>" + tel + "</td>" +
+            "<td>" + email + "</td>" +
+            "<td>" + classes + "</td>" +
+            "<td><a href='#' name='edit'><i class='fas fa-edit'></i></a></td>" +
+            "<td><a href='#' name='delete'><i class='fas fa-times'></i></a></td>" +
+            "</tr>");
+        
+        $('#txtName').val("");
+        $('#datBirthDay').val("");
+        $('#txtAddress').val("");
+        $('#numTel').val("");
+        $('#emlEmail').val("");
+        $('#txtPersonal').val("");
+        $("input[name=sport]").prop("checked", false);
+        $('#classes').val($("#classes option:first").val());
+
+        alert("Added successfully!");
+
+        //xoa ban ghi
+        deleteStudents();
+
+        //tim kiem ban ghi
+        searchStudents();
+
+        //sua ban ghi
+        editStudents();
+
+        //thoat
+        cancelStudents();
+
+        //Validate input
+        validateStudents();
+
+    });
+
+}
+
+//Hàm thoát
+function cancelStudents() {
+    $('#btnCancel').click(function () {
+        $('#txtName').val("");
+        $('#datBirthDay').val("");
+        $('#txtAddress').val("");
+        $('#numTel').val("");
+        $('#emlEmail').val("");
+        $('#txtPersonal').val("");
+        $("input[name=sport]").prop("checked", false);
+        $('#classes').val($("#classes option:first").val());
+
+        $('#btnSave').show();
+        $('#btnUpdate').hide();
+    });
+}
+
+//Hàm chỉnh sửa
+function editStudents() {
+
+    let trEdit;
+
+    //bat su kien khi nhap chuot vao Edit
+    $('a[name=edit]').click(function () {
+
+        //$('#form-dialog').dialog('open');
+
+        trEdit = $(this).closest('tr');
+        $('#btnSave').hide();
+        $('#btnUpdate').show();
+
+        let name = $(trEdit).find('td:eq(0)').text();
+        let dob = $(trEdit).find('td:eq(1)').text();
+        let sex = $(trEdit).find('td:eq(2)').text();
+        let add = $(trEdit).find('td:eq(3)').text();
+        let tel = $(trEdit).find('td:eq(4)').text();
+        let email = $(trEdit).find('td:eq(5)').text();
+        //let personal = $(trEdit).find('td:eq(6)').text();
+        //let selectedSport = $(trEdit).find('td:eq(7)').text();
+        let classes = $(trEdit).find('td:eq(6)').text();
+
+        //alert(selectedSport);
+        //alert(classes);
+        //var lstSelectedSport = selectedSport.split(",");
+
+        $('#txtName').val(name);
+        $('#datBirthDay').val(dob);
+        $('input[name=sex][value=' + sex + ']').prop("checked", true);
+        $('#txtAddress').val(add);
+        $('#numTel').val(tel);
+        $('#emlEmail').val(email);
+        //$('#txtPersonal').val(personal);
+        //for (var item in lstSelectedSport) {
+        //    $('input[   name=sport][value=' + lstSelectedSport[item] + ']').prop("checked", true);
+        //}
+
+        $("#classes").val(classes);
+
+
+    });
+
+    //bat su kiem khi nhap chuot vao update
+    $('#btnUpdate').click(function () {
+        if (trEdit) {
+            //lay du lieu da duoc chinh sua
+            let name = $('#txtName').val();
+            let dob = $('#datBirthDay').val();
+            let sex = $('input[name=sex]:checked').val();
+            let add = $('#txtAddress').val();
+            let tel = $('#numTel').val();
+            let email = $('#emlEmail').val();
+            //let personal = $('#txtPersonal').val();
+            //let selectedSport = [];
+            //$('input[name=sport]:checked').each(function () {
+            //    selectedSport.push(this.value);
+            //});
+            let classes = $("#classes option:selected").val();
+
+            //thay the du lieu cu sau khi chinh sua
+            $(trEdit).find('td:eq(0)').text(name);
+            $(trEdit).find('td:eq(1)').text(dob);
+            $(trEdit).find('td:eq(2)').text(sex);
+            $(trEdit).find('td:eq(3)').text(add);
+            $(trEdit).find('td:eq(4)').text(tel);
+            $(trEdit).find('td:eq(5)').text(email);
+            //$(trEdit).find('td:eq(6)').text(personal);
+            //$(trEdit).find('td:eq(7)').text(selectedSport);
+            $(trEdit).find('td:eq(6)').text(classes);
+
+            trEdit = null;
+
+            $('#txtName').val("");
+            $('#datBirthDay').val("");
+            $('#txtAddress').val("");
+            $('#numTel').val("");
+            $('#emlEmail').val("");
+            $('#txtPersonal').val("");
+            $("input[name=sport]").prop("checked", false);
+            $('#classes').val($("#classes option:first").val());
+
+            $('#btnSave').show();
+            $('#btnUpdate').hide();
+
+            //$('form-dialog').dialog('close');
+            //$('#form-dialog').dialog('close');
+
+        }
+
+    });
+
+}
